101384ff4472300978be7ffa91828b072db468fd
JeriJamz <100004627+JeriJamz@users.noreply.github.com>
2022-02-22 12:11:48 -0800

    Add files via upload

---
+++ Magic 8-Ball.py
@@ -0,0 +1,19 @@
+import random
+#ok guess ima call this list activities
+#OK so this is the magic 8 ball game but gahdamn its gonna be list act 1
+
+messages = ['It is certain',
+            'It is decidedly so',
+            'yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concentrate and ask again',
+            'My reply is no',
+            'Outlook not so good',
+            'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
+
+
+#ok so this activity refers to indention exception
+#this code is kinda weird now it is a list that you can print but like the bottem part is for gahdamn me to one get an index then gahdamnn gives you an random value bc its actuly coded for that
