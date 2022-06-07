8c5efd9c2b15c0da5ceac8d0d24058520e74de7e
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2022-06-06 21:32:00 -0500

    Add files via upload

---
+++ Java2.js
@@ -0,0 +1,7 @@
+var true_or_false = false
+var First_name = 'Johnny'
+var $hydrate = 'water';
+
+document.write('The first name is' + First_name)
+document.write('Is true?' + true_or_false)
+consolelog.write('is this working?') 
---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("count-el").innertext = 5 
+
+let count = 5
+
+log.console(count)
