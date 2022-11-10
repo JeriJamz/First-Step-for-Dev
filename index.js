0b0703ff68e432e279e045b1eecfb6aa63e6ea05
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2022-11-10 10:12:25 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-document.getElementById("count-el").innertext = 5 
+//add js to the body
+console.log('Hello World');//way to add string
+let name = 'Jeri James';
+console.log(name);
+let x = 1;
+let z = 2;
+console.log(x,z);
+let y = x + z;
+console.log(y)
+y = x;
+console.log(y);
+ const intrest_rate = .3;
+function sayHello(){
+    alert("Hallo World")
+}
 
-let count = 5
-
-log.console(count)
---
+++ objore.js
@@ -0,0 +1,15 @@
+//{} opens up for strings litteral
+const circle = {// var circle will never change values
+    radius: 1,
+    loacation: {
+        X:1,
+        y:1
+    }, 
+    draw: function(){// u can say function to make things functions, draw is a method
+        console.log('draw');// the 'draw' should get it to draw
+    }
+};
+
+circle.draw();
+
+//behavior are obj with one or more methods
