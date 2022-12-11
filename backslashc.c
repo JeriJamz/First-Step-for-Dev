87eef54a6de216b3b7f2d6dcc992476b24451b8b
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2022-12-11 10:11:18 -0600

    Add files via upload

---
+++ backslashc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Just testing some new stuff \n");// gotta remember to use double qoutes
+    printf("see if im working this right\n");
+    printf("Boom all good");
+}
