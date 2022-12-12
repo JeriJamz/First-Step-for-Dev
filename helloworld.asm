1b58bec8b5435bdeea87db23df05bbcb658705cb
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2022-12-12 06:20:45 -0600

    we stepped in the deep-end boys

---
+++ helloworld.asm
@@ -0,0 +1,8 @@
+.386
+.model flat, stdcall
+option casemap :none
+
+include \masm32\include\kernel32.inc
+include \masm32\include\masm32.inc
+includelib \masam32\lib\kernel32.lib
+includelib \masam32\lib\masm32.lib 
